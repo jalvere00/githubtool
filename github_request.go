9b89cbb53cf2919f6cf3fb188db9f357c46dad5a
Jameel J Alverez <jamalvere@gmail.com>
2023-06-16 22:25:51 -0400

    Update github_request.go

--- github_request.go
+++ github_request.go
@@ -3,16 +3,36 @@ package main
 import (
 	"net/http"
 	"flag"
+	"encoding/json"
 )
 
 const (
-	base
+	base = "https://api.github.com/repos/"
 )
+
+type release struct {
+	Name string
+	Date string
+}
 
 var token = flag.String("token", "", "Using for Authentication of private repos.")
 
-func GetRepoRelease(user, repository string)  {
+func GetRepoRelease(user, repository string) (release, error) {
 	client := &http.Client{}
+	url := base + fmt.Sprintf("%s%s/releases", user, repository)
 
-	request, err := http.NewRequest("GET", "")
+	request, err := http.NewRequest("GET", url)
+	if err := nil {
+		fmt.Println("Error creating request: ", err)
+		return nil, err
+	}
+
+	response, err := client.Do(request)
+	if err != nil {
+		fmt.Println("Error making response: ", err)
+		return nil, err
+	}
+
+	defer response.Body.Close()
+	
 }
